ea3e74abcae0b1bdf03cc6424b2751e12fa895ed
RODRIGO <j.rodrigo.a.colque@gmail.com>
2024-06-22 20:03:38 -0400

    sextocommit

--- src/components/product/slideshow/ProductSlideshow.tsx
+++ src/components/product/slideshow/ProductSlideshow.tsx
@@ -25,7 +25,6 @@ interface Props {
 
 export const ProductSlideshow = ( { images, title, className }: Props ) => {
 
-
   const [ thumbsSwiper, setThumbsSwiper ] = useState<SwiperObject>();
 
   return (
